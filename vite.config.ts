f8e684aff17d8de6447386297a235a4310643304
Christian <christian@pleno.digital>
2022-10-13 00:41:51 -0500

    ci: add github actions template

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts']
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      reporter: ['lcov', 'text']
+    }
   }
 })
