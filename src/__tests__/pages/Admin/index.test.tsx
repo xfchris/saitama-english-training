679ab589b38dbba896964ab4a22d85b4edf425fd
Christian <christian@pleno.digital>
2022-10-14 22:47:22 -0500

    test: check that the manage page is reenderized

--- src/__tests__/pages/Admin/index.test.tsx
+++ src/__tests__/pages/Admin/index.test.tsx
@@ -1,11 +1,19 @@
 import { screen } from '@testing-library/react'
 import { describe, it } from 'vitest'
-import { trans } from '../../../config/i18n'
+import { memoryDBFirebase } from '../../mocks/FirestoreMemoryMock'
 import { renderAppWithRoute } from '../../helpers'
+import { trans } from '../../../config/i18n'
+import { getWords } from '../../../redux/actions'
+import store from '../../../redux/store'
 
 describe('App', () => {
+  beforeAll(async () => {
+    await store.dispatch(getWords())
+  })
+
   it('should work as expected', () => {
     renderAppWithRoute('/admin/words')
+    expect(screen.getByText(memoryDBFirebase.words[0].data().english)).toBeInTheDocument()
     expect(screen.getByText(trans('label.wordsManage'))).toBeInTheDocument()
   })
 })
